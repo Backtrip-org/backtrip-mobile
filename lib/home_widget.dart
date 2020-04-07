e674786939da9098e32e049d0aa7fea522cc18d3
csgrdcelia <casagrande.celia@gmail.com>
2020-04-07 20:14:43 +0200

    Create BottomNavigationBar with improper colors

---
+++ lib/home_widget.dart
@@ -0,0 +1,57 @@
+import 'package:backtrip/temp_widget.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeState();
+  }
+}
+
+class _HomeState extends State<Home> {
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    PlaceholderWidget(Colors.white),
+    PlaceholderWidget(Colors.deepOrange),
+    PlaceholderWidget(Colors.green),
+    PlaceholderWidget(Colors.blue)
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Backtrip'),
+      ),
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onTabTapped,
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.home),
+            title: new Text('Accueil'),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.timeline),
+            title: new Text('Timeline'),
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.chat_bubble),
+              title: Text('Chat')
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.attach_money),
+              title: Text('Dépenses')
+          )
+        ],
+      ),
+    );
+  }
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+}
