f1bace4c3702ce091d749b7cbe591d8577ebc7f9
Daxxxan <alexisbenard038@gmail.com>
2020-04-13 17:40:43 +0200

    login page structure

--- lib/util/backtrip_api.dart
+++ lib/util/backtrip_api.dart
@@ -1,4 +1,4 @@
 class BacktripApi {
   static final String path = 'http://10.0.2.2:5000';
-  static final String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1ODY3ODEwNzQsImlhdCI6MTU4NjY5NDY2OSwic3ViIjoxfQ.THOO_CVkfIgzz6nb9MTzHC93NbtVCfhlHdFnghvPpQI"; // TODO: Retrieve dynamically
+  static final String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1ODY4NzIyOTYsImlhdCI6MTU4Njc4NTg5MSwic3ViIjoxfQ.jKR3RoREZFwRigxxKYXGTLS_foH0YkkiBvjZ_Gr0_aY"; // TODO: Retrieve dynamically
 }
