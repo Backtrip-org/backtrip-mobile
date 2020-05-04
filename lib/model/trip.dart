64f4ad063ace2252d6e59122afe907c0d25bf876
Vincent Guénin <guenin.vincent4@gmail.com>
2020-05-04 20:32:01 +0200

    Add mapping of participant of trip

--- lib/model/trip.dart
+++ lib/model/trip.dart
@@ -1,17 +1,26 @@
+import 'package:backtrip/model/user.dart';
+
 class Trip {
   int id;
   String name;
   String picturePath;
   int creatorId;
+  List<User> participants;
 
-  Trip({this.id, this.name, this.picturePath, this.creatorId});
+  Trip({this.id, this.name, this.picturePath, this.creatorId, this.participants});
 
-  factory Trip.fromJson(Map<String, dynamic> json) {
+  factory Trip.fromJson(dynamic json) {
+    var participantsJson = json['users_trips'] as List;
+    List<User> _participants = participantsJson
+        .map((user) => User.fromJson(user))
+        .toList();
+
     return Trip(
         id: json['id'],
         name: json['name'],
         picturePath: json['picture_path'],
-        creatorId: json['creator_id']
+        creatorId: json['creator_id'],
+        participants: _participants
     );
   }
 }
