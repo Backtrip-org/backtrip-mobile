08a48eb749c41bad7c4ad80866439825cd9eae83
csgrdcelia <casagrande.celia@gmail.com>
2020-05-19 14:20:21 +0200

    Add logout

--- lib/util/stored_token.dart
+++ lib/util/stored_token.dart
@@ -2,12 +2,17 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class StoredToken {
   static final storage = new FlutterSecureStorage();
+  static final String tokenKey = 'token';
 
   static storeToken(token) {
-    storage.write(key: 'token', value: token);
+    storage.write(key: tokenKey, value: token);
+  }
+
+  static delete() {
+    storage.delete(key: tokenKey);
   }
 
   static Future<String> getToken() {
-    return storage.read(key: 'token');
+    return storage.read(key: tokenKey);
   }
 }
