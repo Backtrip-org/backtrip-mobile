b585cae22b28c8bf11d0333c4bf75bfeb6d9f53d
Daxxxan <alex-bad97@hotmail.fr>
2020-07-09 14:19:23 +0200

    bugfix: set date pickers to french mode

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:backtrip/util/notification.dart';
 import 'package:backtrip/view/authentification/splash_screen_widget.dart';
 import 'package:backtrip/view/theme/backtrip_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() => runApp(App());
 
@@ -10,6 +11,14 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     NotificationManager.initializeNotification();
     return MaterialApp(
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('fr', 'FR')
+      ],
       title: 'Backtrip',
       home: SplashScreenWidget(),
       theme: new BacktripTheme().theme,
